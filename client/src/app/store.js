ebfcec41cf7d875439552e8cc28e28b0aef681be
filipslunsky <fslunsky@gmail.com>
2025-02-01 02:51:44 +0100

    store.js set up and added to main.jsx via Provider

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userSlice from '../features/user/state/slice.js';
-import chatsSlice from '../features/chat/state/slice.js';
-import socketSlice from '../features/chat/state/socketSlice.js';
 
 const appReducer = combineReducers({
     user: userSlice,
-    chats: chatsSlice,
-    socket: socketSlice,
+
 });
 
 const store = configureStore({

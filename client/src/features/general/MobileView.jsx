051ca000d5c7ad4570faff99a82fcb1a60ad2c6e
filipslunsky <fslunsky@gmail.com>
2025-02-05 17:31:30 +0100

    added elements to MobileView.jsx

--- client/src/features/general/MobileView.jsx
+++ client/src/features/general/MobileView.jsx
@@ -1,7 +1,36 @@
+import { useSelector } from "react-redux";
+import nextIcon from '../../assets/img/icon-arrow-right.svg';
+
 const MobileView = () => {
+    const BASE_URL = `${import.meta.env.VITE_API_URL}`;
+
+    const user = useSelector(state => state.user.user);
+    const links = useSelector(state => state.links.currentLinks);
+
     return (
         <>
-            <h2>This is mobile view</h2>
+            <div className="mobileViewMainContainer">
+                <div className="mobileViewUserContainer">
+                    <img src={`${BASE_URL}${user.profilePicture}`} alt="user picture" className="mobileMenuProfilePicture" />
+                    <p className="mobileViewUserName">{user.firstName} {user.lastName}</p>
+                    <p className="mobileViewUserEmail">{user.publicEmail}</p>
+                </div>
+                <div className="mobileViewLinksContainer">
+                    {
+                        links.map(item => {
+                            return (
+                                <div className="mobileViewLinkItem" key={item.display_order}>
+                                    <img src="" alt="link icon" className="mobileViewLinkIcon" />
+                                    <span className="mobileViewLinkItemName">{item.title}</span>
+                                    <button className="mobilViewLinkItemNextButton">
+                                        <img src={nextIcon} alt="link icon" className="mobileViewNextIcon" />
+                                    </button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </>
     );
 }

f1f6a2d32d352ac991bb6c2bff6da81a47aaa939
filipslunsky <fslunsky@gmail.com>
2025-02-10 17:36:23 +0100

    added icons to MobileView.jsx

--- client/src/features/general/MobileView.jsx
+++ client/src/features/general/MobileView.jsx
@@ -1,11 +1,43 @@
 import { useSelector } from "react-redux";
 import nextIcon from '../../assets/img/icon-arrow-right.svg';
+import CodePen from '../../assets/img/icon-codepen.svg';
+import CodeWars from '../../assets/img/icon-codewars.svg';
+import DevTo from '../../assets/img/icon-devto.svg';
+import Facebook from '../../assets/img/icon-facebook.svg';
+import FreeCodeMap from '../../assets/img/icon-freecodecamp.svg';
+import GitHub from '../../assets/img/icon-github.svg';
+import GitLab from '../../assets/img/icon-gitlab.svg';
+import HashNode from '../../assets/img/icon-hashnode.svg';
+import LinkedIn from '../../assets/img/icon-linkedin.svg';
+import StackOverflow from '../../assets/img/icon-stack-overflow.svg';
+import Twitch from '../../assets/img/icon-twitch.svg';
+import Twitter from '../../assets/img/icon-twitter.svg';
+import YouTube from '../../assets/img/icon-youtube.svg';
+import PortfolioWeb from '../../assets/img/icon-profile-details-header.svg';
 
 const MobileView = () => {
     const BASE_URL = `${import.meta.env.VITE_API_URL}`;
 
     const user = useSelector(state => state.user.user);
     const links = useSelector(state => state.links.currentLinks);
+    console.log(links);
+    
+    const icons = {
+        "CodePen": CodePen,
+        "CodeWars": CodeWars,
+        "DevTo": DevTo,
+        "Facebook": Facebook,
+        "FreeCodeCamp": FreeCodeMap,
+        "GitHub": GitHub,
+        "GitLab": GitLab,
+        "HashNode": HashNode,
+        "LinkedIn": LinkedIn,
+        "StackOverflow": StackOverflow,
+        "Twitch": Twitch,
+        "Twitter": Twitter,
+        "YouTube": YouTube,
+        "Portfolio": PortfolioWeb
+    };
 
     return (
         <>
@@ -20,7 +52,7 @@ const MobileView = () => {
                         links.map(item => {
                             return (
                                 <div className="mobileViewLinkItem" key={item.display_order}>
-                                    <img src="" alt="link icon" className="mobileViewLinkIcon" />
+                                    <img src={icons[item.title] || PortfolioWeb} alt="link icon" className="mobileViewLinkIcon" />
                                     <span className="mobileViewLinkItemName">{item.title}</span>
                                     <a className="mobileViewLinkItemNextUrl" href={`https://${item.url}`} target="_blank">
                                         <img src={nextIcon} alt="link icon" className="mobileViewNextIcon" />
--- client/src/features/links/LinkItem.jsx
+++ client/src/features/links/LinkItem.jsx
@@ -1,8 +1,22 @@
 import { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useSortable } from "@dnd-kit/sortable";
-import dragNDropIcon from '../../assets/img/icon-drag-and-drop.svg';
 import { updateLink, deleteLink } from './state/slice.js';
+import dragNDropIcon from '../../assets/img/icon-drag-and-drop.svg';
+import CodePen from '../../assets/img/icon-codepen.svg';
+import CodeWars from '../../assets/img/icon-codewars.svg';
+import DevTo from '../../assets/img/icon-devto.svg';
+import Facebook from '../../assets/img/icon-facebook.svg';
+import FreeCodeMap from '../../assets/img/icon-freecodecamp.svg';
+import GitHub from '../../assets/img/icon-github.svg';
+import GitLab from '../../assets/img/icon-gitlab.svg';
+import HashNode from '../../assets/img/icon-hashnode.svg';
+import LinkedIn from '../../assets/img/icon-linkedin.svg';
+import StackOverflow from '../../assets/img/icon-stack-overflow.svg';
+import Twitch from '../../assets/img/icon-twitch.svg';
+import Twitter from '../../assets/img/icon-twitter.svg';
+import YouTube from '../../assets/img/icon-youtube.svg';
+import PortfolioWeb from '../../assets/img/icon-profile-details-header.svg';
 
 const LinkItem = ({display_order, title, url, index}) => {
     const dispatch = useDispatch();
@@ -47,11 +61,11 @@ const LinkItem = ({display_order, title, url, index}) => {
                         onChange={handleUpdateLink}
                         >
                             <option value="">select platform</option>
-                            <option value="Facebook">Facebook</option>
+                            <option value="Facebook"><img src={Facebook} alt="icon" />Facebook</option>
                             <option value="LinkedIn">LinkedIn</option>
                             <option value="GitHub">GitHub</option>
                             <option value="YouTube">YouTube</option>
-                            <option value="Portfolio Web">Portfolio Web</option>
+                            <option value="PortfolioWeb">Portfolio Web</option>
                         </select>
                     </div>
                     <div className="linkItemInputContainer">

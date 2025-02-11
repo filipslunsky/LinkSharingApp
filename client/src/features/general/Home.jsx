18dcce763b746f0d989e7195b60087acade2dd5a
filipslunsky <fslunsky@gmail.com>
2025-02-11 12:27:44 +0100

    styling added to Home.jsx

--- client/src/features/general/Home.jsx
+++ client/src/features/general/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './home.css';
 
 const Home = () => {
     const navigate = useNavigate();
